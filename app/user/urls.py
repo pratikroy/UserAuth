a75fd26445480d958d70cdfc7b099f92d38972cc
pratikroy <pratikroy2206@gmail.com>
2020-11-02 12:51:36 -0800

    JWT authentication implemented

--- app/user/urls.py
+++ app/user/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("create/", views.CreateUserView.as_view(), name="create"),
     path('retrieve/', views.ListUserView.as_view(), name='retrieve'),
     path('manage/<str:email>/', views.UserManageView.as_view(), name='manage'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,14 +1,24 @@
 from rest_framework import generics
 
 from user.serializers import UserSerializer
+from user.serializers import UserLoginSerializer
 
 from django.contrib.auth import get_user_model
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser
+
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 
 
 class CreateUserView(generics.CreateAPIView):
     """create a new user in the system"""
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
 
 
 
@@ -17,6 +27,8 @@ class ListUserView(generics.ListAPIView):
     """Create new queryset for all users"""
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JSONWebTokenAuthentication,)
 
 
 
@@ -25,4 +37,29 @@ class UserManageView(generics.RetrieveUpdateDestroyAPIView):
     """Retrieve, Update and Destroy a single user record"""
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JSONWebTokenAuthentication,)
     lookup_field = 'email'
+
+
+
+
+class UserLoginView(generics.RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    authentication_classes = []
+    serializer_class = UserLoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        response = {
+            'success': 'True',
+            'status code': status.HTTP_200_OK,
+            'message': 'User logged in successfully',
+            'token': serializer.data['token']
+        }
+        status_code = status.HTTP_200_OK
+
+        return Response(response, status=status_code)
+
+
